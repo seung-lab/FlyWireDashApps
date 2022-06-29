8ca542b6ecab72aafc00b265e6e12abf030701bf
jaybgager <jay.b.gager@gmail.com>
2022-06-29 16:42:55 -0400

    added refeeder links to partner root id columns

--- flywiredashapps/connectivity/utils.py
+++ flywiredashapps/connectivity/utils.py
@@ -568,6 +568,11 @@ def makePartnerDataFrame(root_id, cleft_thresh, upstream=False, config={}):
         .reset_index(drop=True)
     )
 
+    # converts root ids into markdown-readable refeeder links #
+    partner_df[title_name] = [
+        refeedLink(str(x), config) for x in partner_df[title_name]
+    ]
+
     # needs to be converted to strings or the dash table will round the IDs #
     return partner_df.astype(str)
 
@@ -934,6 +939,21 @@ def portUrl(input_ids, app_choice, config={}):
 
     out_url = base + query
     return out_url
+
+
+def refeedLink(root_id, config={}):
+    """Convert root id into markdown-format refeed link.
+
+    Keyword arguments:
+    root_id -- string-format 18-digit root id
+    config -- dictionary of config settings (default {})
+    """
+
+    base = config.get("con_app_base_url", None)
+    query = "?input_field=" + root_id + "&cleft_thresh_field=50"
+    out_url = base + query
+    markdown_url = "[" + root_id + "](" + out_url + ")"
+    return markdown_url
 
 
 def rootsToNucCoords(roots, config={}):
